d0b59bbc71df62b683c0cca193655df7df579b1f
malik <yasmina.rakhimzhanova@narxoz.kz>
2026-05-06 11:19:22 +0500

    add order class

---
+++ src/main/java/kz/rakhimzhanova/finaljavaproject/entity/Order.java
@@ -0,0 +1,22 @@
+package kz.rakhimzhanova.finaljavaproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String customerName;
+
+    private Double totalPrice;
+
+    private String status;
+}
