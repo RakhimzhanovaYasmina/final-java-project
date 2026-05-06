75ba71ef62fce3fbee8352910b775710cf9de1cb
malik <yasmina.rakhimzhanova@narxoz.kz>
2026-05-06 08:34:35 +0500

    Create Product entity

---
+++ src/main/java/kz/rakhimzhanova/finaljavaproject/entity/Product.java
@@ -0,0 +1,22 @@
+package kz.rakhimzhanova.finaljavaproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private Double price;
+
+    private String description;
+}
