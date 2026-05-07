eeaa25849a66c87cca7ace4b5c175832a30e0b27
malik <yasmina.rakhimzhanova@narxoz.kz>
2026-05-08 00:58:01 +0500

    Fix product service update logic

--- src/main/java/kz/rakhimzhanova/finaljavaproject/service/ProductService.java
+++ src/main/java/kz/rakhimzhanova/finaljavaproject/service/ProductService.java
@@ -1,13 +1,16 @@
 package kz.rakhimzhanova.finaljavaproject.service;
 
 import kz.rakhimzhanova.finaljavaproject.entity.Product;
-import kz.rakhimzhanova.finaljavaproject.repository.ProductRepository;
-import org.springframework.stereotype.Service;
 import kz.rakhimzhanova.finaljavaproject.exception.ResourceNotFoundException;
+import kz.rakhimzhanova.finaljavaproject.repository.ProductRepository;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -16,40 +19,66 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository) {
+
         this.productRepository = productRepository;
     }
 
     public List<Product> getAllProducts() {
+
         return productRepository.findAll();
     }
 
-    public Product createProduct(Product product) {
-        return productRepository.save(product);
-    }
-
-    public void deleteProduct(Long id) {
-        productRepository.deleteById(id);
-    }
-
-    public Product updateProduct(Long id, Product updatedProduct) {
-
-        Product product = productRepository.findById(id)
-                .orElseThrow();
-
-        product.setTitle(updatedProduct.getTitle());
-        product.setPrice(updatedProduct.getPrice());
-        product.setDescription(updatedProduct.getDescription());
-
-        return productRepository.save(product);
-    }
-
     public Product getProductById(Long id) {
+
         return productRepository.findById(id)
+
                 .orElseThrow(() ->
+
                         new ResourceNotFoundException(
                                 "Product not found with id " + id
                         )
                 );
+    }
+
+    public Product createProduct(Product product) {
+
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product updatedProduct) {
+
+        Product product =
+                productRepository.findById(id)
+
+                        .orElseThrow(() ->
+
+                                new ResourceNotFoundException(
+                                        "Product not found"
+                                )
+                        );
+
+        product.setTitle(updatedProduct.getTitle());
+
+        product.setDescription(updatedProduct.getDescription());
+
+        product.setPrice(updatedProduct.getPrice());
+
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+
+        Product product =
+                productRepository.findById(id)
+
+                        .orElseThrow(() ->
+
+                                new ResourceNotFoundException(
+                                        "Product not found"
+                                )
+                        );
+
+        productRepository.delete(product);
     }
 
     public Page<Product> getProductsWithPagination(
@@ -59,8 +88,11 @@ public class ProductService {
     ) {
 
         Pageable pageable = PageRequest.of(
+
                 page,
+
                 size,
+
                 Sort.by(sortBy)
         );
 
@@ -68,6 +100,8 @@ public class ProductService {
     }
 
     public List<Product> searchProductsByTitle(String title) {
-        return productRepository.findByTitleContainingIgnoreCase(title);
+
+        return productRepository
+                .findByTitleContainingIgnoreCase(title);
     }
 }
